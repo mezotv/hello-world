a98348b72bc18df2bf4139c7ad1446a4e49c70d6
R C N <plbwymw@gmail.com>
2022-10-09 17:54:50 +0300

    Hello World in visual basic

---
+++ scripts/visualBasic.vb
@@ -0,0 +1,12 @@
+Imports System 'System is a Namespace  
+Module Hello_Program  
+  
+    Sub Main()  
+  
+        Console.WriteLine("Hello, Welcome to the world of VB.NET")  
+        Console.WriteLine("Press any key to continue...")  
+        Console.ReadKey()  
+  
+    End Sub  
+  
+End Module  
