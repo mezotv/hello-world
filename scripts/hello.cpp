f34e365188cd528ce06a41ff222fbbe4ddb216dd
OMKAR <ramishteomkar@gmail.com>
2022-10-09 18:08:07 +0530

    Hello World Program

---
+++ scripts/hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void print(){
+    int pullrequest =0;
+    int merged = 0;
+    if(pullrequest == merged){
+        cout<<"Hello World"<<endl;
+    }
+}
+int main(){
+
+    print();
+    return 0;
+}
